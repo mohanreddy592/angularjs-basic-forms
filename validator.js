d3da7c02c95058229a9fb1e12401745a7d71abcf
mohanreddy592 <mohanreddy592@gmail.com>
2017-12-13 12:40:44 +0530

    added comment

--- validator.js
+++ validator.js
@@ -1,13 +1,13 @@
 myapp.directive('customValidator', function(){
     return {
         restrict: 'A',
-        require: 'ngModel',
+        require: 'ngModel', // you can request multiple directives in array ['d1', 'd2']
         link: function(scope, element, attrs, ngModelCtrl) {
             // here ctrl is directive controller which is added in required
             //console.log(ngModelCtrl);
             ngModelCtrl.$validators.isNameValid = function(val){
                 //console.log(val);
-                if(val && val.startsWith('a')) {
+                if(val && (val.startsWith('a')||val.startsWith('A')))    {
                     return false;
                 }
                 return true;
