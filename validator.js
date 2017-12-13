bd8ef9380e782d2a8718cb8f7116bd7b346b22c7
mohanreddy592 <mohanreddy592@gmail.com>
2017-12-13 12:32:01 +0530

    validator param change

--- validator.js
+++ validator.js
@@ -5,7 +5,7 @@ myapp.directive('customValidator', function(){
         link: function(scope, element, attrs, ngModelCtrl) {
             // here ctrl is directive controller which is added in required
             //console.log(ngModelCtrl);
-            ngModelCtrl.$validators.customValidator = function(val){
+            ngModelCtrl.$validators.isNameValid = function(val){
                 //console.log(val);
                 if(val && val.startsWith('a')) {
                     return false;
